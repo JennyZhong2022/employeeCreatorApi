0f63b3bedaf82b8a493b7730e69ce0b3419c5cd1
Xiao Zhong <zhongxiao2017@gmail.com>
2024-10-07 17:40:36 +1100

    update webconfig

--- src/main/java/nology/employee/config/WebConfig.java
+++ src/main/java/nology/employee/config/WebConfig.java
@@ -6,9 +6,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  @Override
   public void addCorsMappings(CorsRegistry registry) {
-    String[] allowedOrigins = { "http://localhost:5173", "http://127.0.0.1:5173",
-        "https://employee-creator-frontend-dqadefbzc2bedzfj.australiasoutheast-01.azurewebsites.net" };
-    registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("*").allowedHeaders("*");
+    String[] allowedOrigins = {
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://purple-water-0e7163400.5.azurestaticapps.net"
+    };
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods("*")
+        .allowedHeaders("*");
   }
 }
